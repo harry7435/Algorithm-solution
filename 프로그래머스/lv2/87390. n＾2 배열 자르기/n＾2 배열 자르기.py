f0788de329e38dc4f09679dae955b3a377ebc078
Cho_Ik_Jun <70748442+harry7435@users.noreply.github.com>
2022-11-10 14:52:15 +0900

    [level 2] Title: n^2 배열 자르기, Time: 42.70 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,20 +1,11 @@
 def solution(n, left, right):
     answer = []
-    arr = []
-    # i행(n * i) j열을 숫자 i로 채우고 1차원으로 변형하는 것을 한 번에 수행
-    # for i in range(n):
-    #     for j in range(n):
-    #         if i >= j:
-    #             arr[n * i + j] = i + 1  # n * i + j로 변환
-    #         else: arr[n * i + j] = j + 1
-            
     # i행 = idx // n, j열 idx % n으로 치환가능
-    # i행 j열을 숫자 i로 채우고 1차원으로 변형하는 것을 한 번에 수행
+    # i행 j열을 숫자 i로 채우고 1차원으로 변형하여 추가하는 것을 한 번에 수행
     # left부터 right까지만 수행
     for idx in range(left, right+1):
         if idx // n >= idx % n:
-            arr.append(idx // n + 1) 
-        else: arr.append(idx % n + 1)
+            answer.append(idx // n + 1) 
+        else: answer.append(idx % n + 1)
         
-    answer = arr
     return answer
